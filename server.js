fc788378248d736edf497c0c086fab7c1dfd657e
qazi112 <qarsalan13@gmail.com>
2022-06-09 10:33:46 +0500

    Server Added: It's up and running

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const http = require("http");
+const products = require("./data/products")
 
 const server = http.createServer((req, res) => {
+    console.log(req);
+    if (req.url === "/api/products" && req.method == "GET"){
+        const options = {
+            "Content-Type" : "application/json" 
+        }
+        res.writeHead(200,options)
+        res.write(JSON.stringify(products))
+        res.end()
+    }else{
+        res.writeHead(404, "Not Found", {"Content-Type": "text/html"});
+        res.end("Not Found")
+    }
+    
+
 
 });
 
